c76f660430ed803b93b1d8894283012e0ca0538c
logananglin98 <logananglin98@gmail.com>
2024-11-05 22:32:16 -0500

    I believe I have finished the assignment.

--- tlb.c
+++ tlb.c
@@ -1,5 +1,19 @@
+/*
+
+1. The timer I used is precise down to 0.000001 seconds. Using 4096 as the page size and 100000000 as the number of iterations, I can get accurate results with only one page.
+
+2. I believe I did this right, though my graph with Codespaces looks different from the way it looks in the chapter.
+
+5. Adding a print statement after the loop may force the compiler to run the loop every time.
+
+6. Setting the affinity for VSCode to 1 in Task Manager may accomplish this. Using pthread_setaffinity_np can be used in the code to accomplish the same thing. If the code is allowed to move, it may result in more TLB misses than if it were to stay on the same core because the TLB would have to be created again when a process is sent to a core it hasn't ran on. 
+
+7. Commenting out the array initialization loop, getting the results, and then uncommenting to check the results again did not create any meaningful change.
+
+*/
+
 #include <stdlib.h>
-#include <stdio.h>
+#include <stdio.h>S
 #include <time.h>
 
 int main(int argc, char *argv[]) {
